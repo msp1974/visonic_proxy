f02a14ea56729bc73f0c6634cf4343ed1064e16d
Mark Parker <msparker@sky.com>
2024-10-31 18:19:42 +0000

    support config options for add-on

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/with-contenv bashio
+CONFIG_PATH=/data/options.json
 
-#cd /visonic_proxy || exit
+PROXY_MODE="$(bashio::config 'proxy_mode')"
+WEBSOCKET_MODE="$(bashio::config 'websocket_mode')"
+WEBSOCKET_HOST="$(bashio::config 'websocket_host')"
+
+cd /visonic_proxy || exit
 python3 /visonic_proxy/run.py
